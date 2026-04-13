98a114bdfdb439a8150fef3711376d9d57b2e96f
lifeengineer2069 <lifeengineer2069@gmail.com>
2026-04-13 03:59:21 -0400

    ros controller and sim mode update

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     #robot_description_config = xacro.process_file(xacro_file).toxml()
     robot_description_config = ParameterValue(
-        Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control]),
+        Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time]),
         value_type=str
     )
 
